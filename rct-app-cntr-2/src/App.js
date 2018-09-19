0e549a0d249c5ae93d8e20a3cae65fe27106ea5d
jbmominee <jaymominee@gmail.com>
2018-09-19 09:31:25 -0500

    initial build of counter app, state not visible

--- rct-app-cntr-2/src/App.js
+++ rct-app-cntr-2/src/App.js
@@ -1,11 +1,42 @@
 import React, { Component } from 'react';
 import './App.css';
+import Counter from './components/Counter.js';
 
 class App extends Component {
+  constructor(props){
+    super(props);
+      this.state = {
+        value: 1
+      };
+
+  this.handleUpClick = this.handleUpClick.bind(this);
+  this.handleDownClick = this.handleDownClick.bind(this);
+  this.handleReset = this.handleReset.bind(this);
+
+    }
+
+  handleUpClick(){
+    let upClick = this.state.value + 1;
+    this.setState({value: upClick });
+  }
+
+  handleDownClick(){
+    let downClick = Math.max(-10, this.state.value - 1);
+    this.setState({value: downClick});
+  }
+
+  handleReset(){
+    this.setState({value: 0});
+  }
+
+
   render() {
     return (
       <div className="App">
-        
+        <Counter
+          upClick={this.handleUpClick}
+          downClick={this.handleDownClick}
+          resetClick={this.handleReset} />
       </div>
     );
   }
--- rct-app-cntr-2/src/components/Counter.js
+++ rct-app-cntr-2/src/components/Counter.js
@@ -1,1 +1,22 @@
 import React, { Component } from 'react';
+
+class Counter extends Component {
+  constructor(props){
+    super(props);
+
+
+    }
+
+  render(){
+    return(
+      <div>
+        <h1>Value: {this.props.value}</h1>
+        <button onClick={this.props.handleUpClick}>Up</button>
+        <button onClick={this.props.handleDownClick}>Down</button>
+        <button onClick={this.props.handleReset}>Reset</button>
+      </div>
+    );
+  }
+}
+
+export default Counter;
